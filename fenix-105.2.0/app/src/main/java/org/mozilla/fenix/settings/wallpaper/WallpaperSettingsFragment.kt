68f2e008aaae4a443b8b3bd412fa6e6a0e6ff6f1
V_Ramkumar <V_Ramkumar@hcl.com>
2022-10-11 19:12:03 +0530

    Adding files

---
+++ fenix-105.2.0/app/src/main/java/org/mozilla/fenix/settings/wallpaper/WallpaperSettingsFragment.kt
@@ -0,0 +1,66 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings.wallpaper
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import mozilla.components.lib.state.ext.observeAsComposableState
+import mozilla.components.service.glean.private.NoExtras
+import org.mozilla.fenix.GleanMetrics.Wallpapers
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.requireComponents
+import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.theme.FirefoxTheme
+import org.mozilla.fenix.wallpapers.Wallpaper
+
+class WallpaperSettingsFragment : Fragment() {
+    private val appStore by lazy {
+        requireComponents.appStore
+    }
+
+    private val wallpaperUseCases by lazy {
+        requireComponents.useCases.wallpaperUseCases
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Wallpapers.wallpaperSettingsOpened.record(NoExtras())
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                FirefoxTheme {
+                    val wallpapers = appStore.observeAsComposableState { state ->
+                        state.wallpaperState.availableWallpapers
+                    }.value ?: listOf()
+                    val currentWallpaper = appStore.observeAsComposableState { state ->
+                        state.wallpaperState.currentWallpaper
+                    }.value ?: Wallpaper.Default
+
+                    WallpaperSettings(
+                        wallpapers = wallpapers,
+                        defaultWallpaper = Wallpaper.Default,
+                        loadWallpaperResource = { wallpaperUseCases.loadBitmap(it) },
+                        selectedWallpaper = currentWallpaper,
+                        onSelectWallpaper = { wallpaperUseCases.selectWallpaper(it) },
+                        onViewWallpaper = { findNavController().navigate(R.id.homeFragment) },
+                    )
+                }
+            }
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        showToolbar(getString(R.string.customize_wallpapers))
+    }
+}
---
+++ fenix-105.2.0/app/src/main/java/org/mozilla/fenix/wallpapers/Wallpaper.kt
@@ -0,0 +1,80 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.wallpapers
+
+import java.util.Date
+
+/**
+ * Type that represents wallpapers.
+ *
+ * @property name The name of the wallpaper.
+ * @property collection The name of the collection the wallpaper belongs to.
+ * is not restricted.
+ * @property textColor The 8 digit hex code color that should be used for text overlaying the wallpaper.
+ * @property cardColor The 8 digit hex code color that should be used for cards overlaying the wallpaper.
+ */
+data class Wallpaper(
+    val name: String,
+    val collection: Collection,
+    val textColor: Long?,
+    val cardColor: Long?,
+) {
+    /**
+     * Type that represents a collection that a [Wallpaper] belongs to.
+     *
+     * @property name The name of the collection the wallpaper belongs to.
+     * @property learnMoreUrl The URL that can be visited to learn more about a collection, if any.
+     * @property availableLocales The locales that this wallpaper is restricted to. If null, the wallpaper
+     * is not restricted.
+     * @property startDate The date the wallpaper becomes available in a promotion. If null, it is available
+     * from any date.
+     * @property endDate The date the wallpaper stops being available in a promotion. If null,
+     * the wallpaper will be available to any date.
+     */
+    data class Collection(
+        val name: String,
+        val heading: String?,
+        val description: String?,
+        val learnMoreUrl: String?,
+        val availableLocales: List<String>?,
+        val startDate: Date?,
+        val endDate: Date?,
+    )
+
+    companion object {
+        const val amethystName = "amethyst"
+        const val ceruleanName = "cerulean"
+        const val sunriseName = "sunrise"
+        const val twilightHillsName = "twilight-hills"
+        const val beachVibeName = "beach-vibe"
+        const val firefoxCollectionName = "firefox"
+        const val defaultName = "default"
+        val DefaultCollection = Collection(
+            name = defaultName,
+            heading = null,
+            description = null,
+            learnMoreUrl = null,
+            availableLocales = null,
+            startDate = null,
+            endDate = null,
+        )
+        val Default = Wallpaper(
+            name = defaultName,
+            collection = DefaultCollection,
+            textColor = null,
+            cardColor = null,
+        )
+
+        /**
+         * Defines the standard path at which a wallpaper resource is kept on disk.
+         *
+         * @param orientation One of landscape/portrait.
+         * @param theme One of dark/light.
+         * @param name The name of the wallpaper.
+         */
+        fun getBaseLocalPath(orientation: String, theme: String, name: String): String =
+            "wallpapers/$orientation/$theme/$name.png"
+    }
+}
